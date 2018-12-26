8f5c1f28eade85a86084fc1ba16b480faca26480
Rina Sergeeva <rina_sergeeva@bk.ru>
2018-12-26 19:35:04 +0300

    start

---
+++ app/src/main/java/ru/sergeeva/rina/togo/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.sergeeva.rina.togo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.sergeeva.rina.togo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
